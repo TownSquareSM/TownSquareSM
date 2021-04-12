506361bdf2e263aa6bb2388b45cb64eca4d0fe7b
inderForProjectTony <inder123.2007@rediffmail.com>
2021-04-12 08:55:14 +0530

    need more admins in the group task

---
+++ components/OssnGroups/actions/group/change_group_admin.php
@@ -0,0 +1,19 @@
+<?php 
+$new_admin = input('user');
+$group     = ossn_get_group_by_guid(input('group'));
+
+if ($group->owner_guid !== ossn_loggedin_user()->guid && !ossn_isAdminLoggedin() && !$group->isUserGroupAdmin($group->guid,ossn_loggedin_user()->guid)) {
+    ossn_trigger_message(ossn_print('group:update:fail'), 'error');
+    redirect(REF);
+}
+
+
+if ($group->makeGroupAdmin($new_admin, $group->guid)) {
+    ossn_trigger_message("User added as Group Admin successfully!!");
+    redirect("group/{$group->guid}/members");
+} else {
+    ossn_trigger_message(ossn_print('group:update:fail'), 'error');
+    redirect(REF);
+}
+
+?>
